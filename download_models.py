713e45d8972e4aac5daaf30e8d798778107b1f7e
susannabravi <susannabravi@gmail.com>
2025-04-23 11:44:22 +0200

    prova download deepseek coder v2

---
+++ download_models.py
@@ -0,0 +1,24 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausalLM
+import os
+
+def download_model_hf(model_path, model_name):
+    """Download a Hugging Face model and tokenizer to the specified directory"""
+    # Check if the directory already exists
+    if not os.path.exists(model_path):
+        # Create the directory
+        os.makedirs(model_path)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+    # Save the model and tokenizer to the specified directory
+    model.save_pretrained(model_path)
+    tokenizer.save_pretrained(model_path)
+
+tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-Coder-V2-Lite-Base", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-Coder-V2-Lite-Base", trust_remote_code=True, torch_dtype=torch.bfloat16).cuda()
+input_text = "#write a quick sort algorithm"
+inputs = tokenizer(input_text, return_tensors="pt").to(model.device)
+outputs = model.generate(**inputs, max_length=128)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
